33c2dd031ae714aedc8e9d692bfc0a492db161a1
gabrielleal23 <110639928+gabrielleal23@users.noreply.github.com>
2025-06-01 18:38:06 -0500

    segunda parte de codificacion

--- Codigo.py
+++ Codigo.py
@@ -7,3 +7,13 @@ def limpiar_texto(texto):
     texto = texto.translate(str.maketrans("", "", string.punctuation))
     return texto
 
+def es_palindromo(texto):
+    texto_limpio =  limpiar_texto(texto)
+    return texto_limpio == texto_limpio[::-1]
+
+if __name__ == "__main__":
+    palabra = input("Escribe una palabra o frase :")
+    if es_palindromo(palabra):
+        print("Es un palindromo")
+    else:
+        print("No es un palindromo")
